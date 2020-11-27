83cbd0cfb633304b98f423a244c62061844889ef
Minininda <Minininda@users.noreply.github.com>
2020-11-27 23:48:48 +0800

    Siberian winter！

---
+++ Minda_Week5_HW_WInter_is_coming!/src/Particle.cpp
@@ -0,0 +1,69 @@
+#include "Particle.h"
+
+Particle::Particle()
+{
+    pos			= glm::vec3(ofRandom(-2000,2000),ofRandom(700,700),ofRandom(-2000,2000));
+	vel.x		= ofRandom(-1,1);
+	vel.y		= ofRandom(-1,1);
+	vel.z		= ofRandom(-1,1);
+	lifespan		= ofRandom(5,10); // seconds
+	mass		    = ofRandom(1,3);
+	lastUpdateTime  = ofGetElapsedTimef();
+}
+
+Particle::Particle(glm::vec3 _pos, glm::vec3 _vel, float _life, float _mass)
+{
+	pos			= _pos;
+	vel			= _vel;
+	lifespan    = _life;
+	mass		= _mass;
+	lastUpdateTime = ofGetElapsedTimef();
+}
+
+void Particle::applyForce(glm::vec3 force)
+{
+	acc += force / mass;
+}
+
+void Particle::applyDrag(float amt)
+{
+    float speed = glm::length(vel);
+    if (speed > 0) {
+        applyForce( -vel / speed * amt );
+    }
+}
+
+void Particle::update()
+{
+	vel += acc;
+	pos += vel;
+	acc *= 0;
+	
+    // update age
+	age += ofGetElapsedTimef() - lastUpdateTime;    // + time diff
+	lastUpdateTime = ofGetElapsedTimef();
+}
+
+void Particle::draw()
+{
+	ofPushStyle();
+	
+	ofColor fire	= ofColor(255, 255, 255);
+	ofColor smoke	= ofColor(96,96,96);
+
+	float lerp		= ofClamp(age/lifespan, 0, 1);
+	ofColor color	= fire.getLerped( smoke, lerp);
+	ofSetColor( color );
+	
+	material.setEmissiveColor( color );
+	material.begin();
+	
+	float radius		= lifespan;
+	
+	ofDrawSphere(pos, radius);
+	
+	material.end();
+	
+	ofPopStyle();
+}
+
---
+++ Minda_Week5_HW_WInter_is_coming!/src/ofApp.cpp
@@ -0,0 +1,160 @@
+#include "ofApp.h"
+
+ofPoint a[5050];
+//--------------------------------------------------------------
+void ofApp::setup(){
+	
+	ofBackground(255);
+
+    for (int i=0;i<=500;i++)
+    {
+        a[i].x=ofRandom(-6000,6000);
+        a[i].y=-200;
+        a[i].z=ofRandom(-6000,6000);
+    }
+	
+	ofSetSmoothLighting(true);
+	ofSetFrameRate(40);
+	volcano.set(250, 400, 80, 70);
+    volcano.rotateDeg(180, glm::vec3(1,0,0));
+	
+	auto& coneVerts = volcano.getMesh().getVertices();
+	ground.set(10000, 10000, 1000, 1000);
+    ground.rotateDeg(-90, glm::vec3(1,0,0));
+	ground.move(0, -200, 0);
+	gravity = ofVec3f(0,-.1,0);
+
+	firelight.setPointLight();
+	firelight.setPosition(0,200,0);
+    firelight.setDiffuseColor( ofColor::white );
+	sunlight.setDirectional();
+	sunlight.setPosition(100,100,-400);
+    sunlight.lookAt(glm::vec3(0));
+	sunlight.setDiffuseColor( ofColor::white);
+	
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	for (int i=0; i<25; i++)
+	{
+		Particle p;
+		p.vel.x = ofRandom(-10,10);
+		p.vel.y = ofRandom(0,0);
+		p.vel.z = ofRandom(-10,10);
+		p.lifespan = ofRandom(1,3);
+		p.mass = ofRandom(.7,1.3);
+		eruption.push_back(p);
+	}
+	
+	while (eruption.size()>3800)
+	{
+		eruption.erase(eruption.begin());
+	}
+	
+	for (int i=0; i<eruption.size(); i++)
+	{
+		eruption[i].applyForce(gravity);
+		eruption[i].update();
+	}
+
+	firelight.setDiffuseColor(255);
+   sunlight.rotateDeg(.2, 0,1,0);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    ofBackgroundGradient(ofColor::gray, ofColor(255));
+	
+	ofEnableLighting();
+	
+	cam.begin();
+	ofEnableDepthTest();
+	
+	firelight.enable();
+	
+	for (int i=0; i<eruption.size(); i++)
+	{
+		eruption[i].draw();
+	}
+	
+	sunlight.enable();
+	
+	ofSetColor( ofColor(73, 48, 35) );
+    ofSetColor( ofColor::white) ;
+	ground.draw();
+	ofSetColor(255);
+	
+	firelight.disable();
+	sunlight.disable();
+    for (int i=0;i<=500;i++)
+    {
+        ofSetColor(102,51,0);
+    
+        ofDrawCylinder(a[i], 30,170);
+        ofSetColor(0,150,0);
+        ofDrawCone(a[i].x, 0, a[i].z, 100, -230);
+        ofDrawCone(a[i].x, 120, a[i].z, 50, -140);
+    }
+	cam.end();
+        
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
